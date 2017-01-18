101bef3d536d4ac9c7833757c4b76b2bcf1a6aa3
Rutabadev <etienner37@gmail.com>
2017-01-18 12:35:41 +0100

    intent Menu → Bureau qui ne fonctionne pas

--- app/src/main/java/com/example/tanguygranat/continental/BureauActivity.java
+++ app/src/main/java/com/example/tanguygranat/continental/BureauActivity.java
@@ -2,13 +2,14 @@ package com.example.tanguygranat.continental;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 
 /**
  * Created by tanguy.granat on 18/01/2017.
  */
 
-public class BureauActivity extends Activity {
+public class BureauActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
