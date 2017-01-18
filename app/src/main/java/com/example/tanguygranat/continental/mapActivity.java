f7797f353f906c3036584d4d730197132a99457b
Rutabadev <etienner37@gmail.com>
2017-01-18 15:25:17 +0100

    navigation

--- app/src/main/java/com/example/tanguygranat/continental/mapActivity.java
+++ app/src/main/java/com/example/tanguygranat/continental/mapActivity.java
@@ -3,11 +3,15 @@ package com.example.tanguygranat.continental;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.visioglobe.visiomoveessential.VMEMapView;
+
 public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        VMEMapView mapView = (VMEMapView) findViewById(R.id.mapView);
+        mapView.loadMap();
     }
 }
