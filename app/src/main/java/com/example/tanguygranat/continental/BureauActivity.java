429f434335bfc36a48dfd25882007409257f8dd7
Kanowki <tanguy31270@hotmail.fr>
2017-01-18 10:52:44 +0100

    test1

---
+++ app/src/main/java/com/example/tanguygranat/continental/BureauActivity.java
@@ -0,0 +1,19 @@
+package com.example.tanguygranat.continental;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by tanguy.granat on 18/01/2017.
+ */
+
+public class BureauActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bureau_activity);
+    }
+
+}
